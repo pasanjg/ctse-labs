aa0a3c08cc9b779cd91d1b3504a6ff22247ac4c9
pasanjg <44201430+pasanjg@users.noreply.github.com>
2020-02-26 02:19:38 +0530

    Lab3: AppMain page

--- Labsheet03/lib/main.dart
+++ Labsheet03/lib/main.dart
@@ -7,6 +7,10 @@ class TrevaShop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          fontFamily: 'AvenirNextLTPro'
+      ),
       home: AppMain(),
     );
   }
--- Labsheet03/lib/pages/app_main.dart
+++ Labsheet03/lib/pages/app_main.dart
@@ -4,9 +4,129 @@ class AppMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: Center(
-        child: Text("Treva Shop"),
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/main_image_smo.jpeg'),
+              colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.5),
+                BlendMode.dstATop,
+              ),
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                "Treva Shop",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          width: 100,
+                          child: Divider(
+                            color: Colors.white70,
+                          ),
+                        ),
+                        Text(
+                          "Get best products in treva shop",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 45.0,
+                      child: FlatButton(
+                        splashColor: Colors.white12,
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(40.0),
+                            side: BorderSide(color: Colors.white60)),
+                        child: Center(
+                          child: Text(
+                            "Signup",
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Flex(
+                        direction: Axis.horizontal,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Divider(
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Expanded(
+                            child: Center(
+                              child: Text(
+                                "OR SKIP",
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Divider(
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 45.0,
+                      child: FlatButton(
+                        splashColor: Colors.white12,
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(40.0),
+                            side: BorderSide(color: Colors.white60)),
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
